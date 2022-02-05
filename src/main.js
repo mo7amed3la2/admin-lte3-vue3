a3c2bce6d3cad667d2ee363d5d4867848a3025f8
Mohamed Alaa <m.3laa.95@gmail.com>
2022-02-05 12:11:59 +0200

    adminlte setup

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
+import "jquery";
+import "bootstrap";
+import "popper.js";
+// fortawesome
+import "@fortawesome/fontawesome-free/js/brands.js";
+import "@fortawesome/fontawesome-free/js/solid.js";
+import "@fortawesome/fontawesome-free/js/fontawesome.js";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+// adminlte css
+import "./assets/dist/css/adminlte.min.css";
+// adminlte js
+import "./assets/dist/js/adminlte.min.js";
+
 createApp(App).mount("#app");
