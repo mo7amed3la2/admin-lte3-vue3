66564245104307ef3f8f57554efea3f6ec6cc67e
Mohamed Alaa <m.3laa.95@gmail.com>
2022-02-05 13:32:56 +0200

    structure main component

---
+++ src/components/global/Mainfooter.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">Dashboard</div>
+    <!-- Default to the left -->
+    <strong
+      >Copyright &copy; {{ date }}
+      <a href="https://adminlte.io">dev3la2.io</a>.</strong
+    >
+    All rights reserved.
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      date: new Date().getFullYear(),
+    };
+  },
+};
+</script>
